c2fd200696fde093f34531bb4c503965842d114d
Pascal Vizeli <pvizeli@syshack.ch>
2019-05-01 10:51:34 +0200

    Fix include

--- builder/__main__.py
+++ builder/__main__.py
@@ -12,11 +12,16 @@ from builder.upload import run_upload
 
 
 @click.command()
-@click.option("--apk", default="build-base", help="APKs they are needed to build this.")
-@click.option("--index", required=True, help="Index URL of remote wheels repository.")
-@click.option("--requirement", required=True, type=click_pathlib.Path, help="Python requirement file.")
-@click.option("--upload", default="rsync", help="Upload plugin to upload wheels.")
-@click.option("--remote", required=True, type=str, help="Remote URL pass to upload plugin.")
+@click.option(
+    "--apk", default="build-base", help="APKs they are needed to build this.")
+@click.option(
+    "--index", required=True, help="Index URL of remote wheels repository.")
+@click.option(
+    "--requirement", required=True, type=click_pathlib.Path(exists=True), help="Python requirement file.")
+@click.option(
+    "--upload", default="rsync", help="Upload plugin to upload wheels.")
+@click.option(
+    "--remote", required=True, type=str, help="Remote URL pass to upload plugin.")
 def builder(apk, index, requirement, upload, remote):
     """Build wheels precompiled for Home Assistant container."""
     install_apks(apk)
