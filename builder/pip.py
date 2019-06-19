df42045f9bbac02d9c33ba1211b80aee8f0a3dd4
Pascal Vizeli <pvizeli@syshack.ch>
2019-06-19 12:22:11 +0000

    Add support local builds

--- builder/pip.py
+++ builder/pip.py
@@ -46,6 +46,26 @@ def build_wheels_requirement(requirement: Path, index: str, output: Path) -> Non
     result.check_returncode()
 
 
+def build_wheels_local(index: str, output: Path) -> None:
+    """Build wheels from a requirements file into output."""
+    cpu = os.cpu_count() or 4
+
+    # Modify speed
+    build_env = os.environ.copy()
+    build_env["MAKEFLAGS"] = f"-j{cpu}"
+
+    result = subprocess.run(
+        f"pip3 wheel --progress-bar ascii --wheel-dir {output} --find-links {index} .",
+        shell=True,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        env=build_env,
+    )
+
+    # Check result of program
+    result.check_returncode()
+
+
 def parse_requirements(requirement: Path) -> List[str]:
     """Parse a requirement files into an array."""
     requirement_list = set()
