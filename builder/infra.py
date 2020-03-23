519c67c9665dff2ceafad8677fb7e5e26d4e30bb
Pascal Vizeli <pvizeli@syshack.ch>
2020-03-23 11:36:46 +0000

    Check skip binary with remote index

--- builder/infra.py
+++ builder/infra.py
@@ -1,7 +1,13 @@
 """Create folder structure for index."""
 from pathlib import Path
+import re
+from typing import List, Set
+
+import requests
 
 from .utils import alpine_version, build_arch
+
+_RE_REQUIREMENT = re.compile(r"(?P<package>.+)(?:==|>|<|<=|>=|~=)(?P<version>.+)")
 
 
 def create_wheels_folder(base_folder: Path) -> Path:
@@ -15,3 +21,28 @@ def create_wheels_folder(base_folder: Path) -> Path:
 def create_wheels_index(base_index: str) -> str:
     """Create wheels specific URL."""
     return f"{base_index}/{alpine_version()}/{build_arch()}/"
+
+
+def check_available_binary(index: str, skip_binary: str, packages: List[str]) -> str:
+    """Check if binary exists and ignore this skip."""
+    if skip_binary == ":none:":
+        return skip_binary
+
+    list_binary = skip_binary.split(",")
+    available_data = requests.get(index).text
+
+    list_needed: Set[str] = set()
+    for binary in list_binary:
+        for package in packages:
+            if not package.startswith(binary):
+                continue
+            find = _RE_REQUIREMENT.fullmatch(package)
+            name = f"{find['package']}-{find['version']}"
+            if available_data.find(name) != -1:
+                continue
+            list_needed.add(binary)
+
+    # Generate needed list of skip binary
+    if not list_needed:
+        return ":none:"
+    return ",".join(list_needed)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = "1.8.0"
+VERSION = "1.9.0"
 
 setup(
     name="builder",
