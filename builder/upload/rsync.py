c5664670c01a63c8a917056ee76c54fbac06e423
Pascal Vizeli <pvizeli@syshack.ch>
2021-05-12 12:39:38 +0200

    Remove partial to avoid corrupt files (#224)

--- builder/upload/rsync.py
+++ builder/upload/rsync.py
@@ -7,5 +7,5 @@ from ..utils import run_command
 def upload(local: Path, remote: str) -> None:
     """Upload wheels from folder to remote rsync server."""
     run_command(
-        f"rsync --human-readable --recursive --partial --progress --checksum {local}/* {remote}/",
+        f"rsync --human-readable --recursive --progress --checksum {local}/* {remote}/",
     )
