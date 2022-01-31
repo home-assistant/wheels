145d41704e4457ad8857b93753bd4f586528e76a
Martin Hjelmare <marhje52@gmail.com>
2022-01-31 17:36:46 +0100

    Run black

--- tests/test_pip.py
+++ tests/test_pip.py
@@ -20,15 +20,12 @@ def test_extract_packages():
 
 
 def test_extract_packages_diff():
-    assert (
-        sorted(
-            pip.extract_packages(
-                Path(__file__).parent / "requirements/requirements_all.txt",
-                Path(__file__).parent / "requirements/requirements_diff.txt",
-            )
+    assert sorted(
+        pip.extract_packages(
+            Path(__file__).parent / "requirements/requirements_all.txt",
+            Path(__file__).parent / "requirements/requirements_diff.txt",
         )
-        == ["RPi.GPIO==1.2.3", "aiohue==5.6.7"]
-    )
+    ) == ["RPi.GPIO==1.2.3", "aiohue==5.6.7"]
 
 
 def test_extract_packages_diff2():
