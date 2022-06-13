ed9581d53293033a92b3715f5a05552282cc74ef
Pascal Vizeli <pvizeli@syshack.ch>
2022-06-13 08:25:53 +0000

    finish audithweel & naming

--- builder/wheel.py
+++ builder/wheel.py
@@ -8,7 +8,10 @@ from tempfile import TemporaryDirectory
 from .utils import run_command, build_arch
 
 
-_RE_WHEEL_PLATFORM = re.compile(r"^(?P<name>.*-)cp\d{2}m?-linux_\w+\.whl$")
+_RE_LINUX_PLATFORM = re.compile(r"-linux_\w+\.whl$")
+_RE_MUSLLINUX_PLATFORM = re.compile(
+    r"-musllinux_(?P<major>\d)_(?P<minor>\d)_(?P<arch>\w+)\.whl$"
+)
 
 _ARCH_PLAT = {
     "amd64": "x86_64",
@@ -19,15 +22,6 @@ _ARCH_PLAT = {
 }
 
 _ALPINE_MUSL_TAG = {"3.16": "musllinux_1_2"}
-
-
-def fix_wheels_name(wheels_folder: Path) -> None:
-    """Remove platform tag from filename."""
-    for package in wheels_folder.glob("*.whl"):
-        match = _RE_WHEEL_PLATFORM.match(package.name)
-        if not match:
-            continue
-        package.rename(Path(package.parent, f"{match.group('name')}none-any.whl"))
 
 
 def copy_wheels_from_cache(cache_folder: Path, wheels_folder: Path) -> None:
@@ -41,11 +35,25 @@ def run_auditwheel(wheels_folder: Path) -> None:
     """Run auditwheel to include shared library."""
     with TemporaryDirectory() as temp_dir:
         for wheel_file in wheels_folder.glob("*.whl"):
-            if "musllinux" in wheel_file.name:
+            if not _RE_LINUX_PLATFORM.match(wheel_file.name):
                 continue
             run_command(f"auditwheel repair -w {temp_dir} {wheel_file}")
 
         # Fix architecture
-        # Qemu armv6 looks like armv7
-        # FIXME
+        # Align to Alpine arch / Qemu issues on CI
         target_arch = _ARCH_PLAT[build_arch()]
+        for wheel_file in Path(temp_dir).glob("*.whl"):
+            package = _RE_MUSLLINUX_PLATFORM.match(wheel_file.name)
+            if not package:
+                raise RuntimeError(f"Wheel format error {wheel_file}")
+            if package["arch"] == target_arch:
+                shutil.copy(wheel_file, wheels_folder)
+            else:
+                fix_name = wheel_file.name.replace(package["arch"], target_arch)
+                shutil.copy(wheel_file, wheels_folder.joinpath(fix_name))
+
+    # Cleanup linux_ARCH tags
+    for wheel_file in wheels_folder.glob("*.whl"):
+        if not _RE_LINUX_PLATFORM.match(wheel_file.name):
+            continue
+        wheel_file.unlink()
