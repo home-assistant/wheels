f73cc47ca09b59a13f09f535b76ad77fe0b2f42f
Stefan Agner <stefan@agner.ch>
2025-09-26 11:17:52 +0200

    Upgrade installed packages before adding more
    Upgrade all installed packages before installing more packages. This
    avoid potential issues with (unintended) ABI breakages between Alpine
    packages.

--- builder/apk.py
+++ builder/apk.py
@@ -10,6 +10,15 @@ def install_apks(apks: str) -> None:
     """Install all apk string formatted as 'package1;package2'."""
     packages = " ".join(apks.split(";"))
 
+    # Upgrade installed packages to their latest version
+    subprocess.run(
+        "apk upgrade --no-cache"
+        shell=True,
+        check=True,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+
     subprocess.run(
         f"apk add --no-progress --no-cache {packages}",
         shell=True,
